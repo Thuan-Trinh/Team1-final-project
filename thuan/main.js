3641d1afbce0b779179038353d45c6c21257a6ed
Thuan-Trinh <thuantrinh7@gmail.com>
2024-03-21 13:31:16 +0700

    handle search bar

--- thuan/main.js
+++ thuan/main.js
@@ -1,108 +1,121 @@
-// sidebar header toggle
-{
-    const sideMenu = document.getElementById('side-menu');
-    const showSideMenu = document.getElementById('toggle-menu');
-    const hideSideMenu = document.getElementById('close-menu');
-    
-    const overlay = document.getElementById('overlay');
-    
-    showSideMenu.onclick = () => {
-        sideMenu.classList.toggle('show-side-menu');
-        overlay.style.display = 'block';
-    }
-    
-    hideSideMenu.onclick = () => {
-        sideMenu.classList.remove('show-side-menu');
-        overlay.style.display = 'none';
-    }
-}
-
-// img catalog array
-// Mảng chứa thông tin của 6 card
-// Mảng chứa thông tin của 6 card
-{
-    let cataCards = [
-        {
-            image: "../assets/images/item-img-1.png",
-            name: "1.3 Chair",
-            price: "$69.90"
-        },
-        {
-            image: "../assets/images/item-img-2.png",
-            name: "Kuyu Desk",
-            price: "$79.90"
-        },
-        {
-            image: "../assets/images/item-img-3.png",
-            name: "Neat Noon",
-            price: "$89.90"
-        },
-        {
-            image: "../assets/images/item-img-4.png",
-            name: "1.3 Chair",
-            price: "$99.90"
-        },
-        {
-            image: "../assets/images/item-img-5.png",
-            name: "Morph",
-            price: "$109.90"
-        },
-        {
-            image: "../assets/images/item-img-6.png",
-            name: "1.3 Chair",
-            price: "$119.90"
-        }
+(function () {
+  {
+    //    Thêm card sản phẩm trong catalogpage
+    // img catalog array
+    const cataCards = [
+      {
+        image: "../assets/images/item-img-1.png",
+        name: "1.3 Chair",
+        price: "$69.90",
+      },
+      {
+        image: "../assets/images/item-img-2.png",
+        name: "Kuyu Desk",
+        price: "$79.90",
+      },
+      {
+        image: "../assets/images/item-img-3.png",
+        name: "Neat Noon",
+        price: "$89.90",
+      },
+      {
+        image: "../assets/images/item-img-4.png",
+        name: "1.3 Chair",
+        price: "$99.90",
+      },
+      {
+        image: "../assets/images/item-img-5.png",
+        name: "Morph",
+        price: "$109.90",
+      },
+      {
+        image: "../assets/images/item-img-6.png",
+        name: "1.3 Chair",
+        price: "$119.90",
+      },
+      // Thêm các phần tử khác vào mảng tương tự như trên
     ];
-    
-    // Gọi phần tử trong card container và grid container
-    let cardContainer = document.querySelector('.card-items');
-    let gridCardsContainer = document.getElementById('grid-item-cards');
-    
-    // gọi card mẫu BÊN TRONG card container
-    let cardTemplate = cardContainer.querySelector('.card');
-    
-    // Dùng forEach(function(){}) để tạo card mới và thêm vào grid
-    /*
-    array.forEach(function(currentValue, index, array) {
-        // Code xử lý cho mỗi phần tử của mảng
-    });
-     */
-    cataCards.forEach(function(cardInfor){
-        // clone card mẫu từ cardTemplate
+
+    const gridCardsContainer = document.getElementById("grid-item-cards");
+    const cardTemplate = document.querySelector(".card");
+
+    if (cardTemplate) {
+      cataCards.forEach(function (cardInfor) {
         let cardClone = cardTemplate.cloneNode(true);
-    
-        // Thêm thông tin vào card clone
-        cardClone.querySelector('.card-img').src = cardInfor.image;
-        cardClone.querySelector('.card-name').textContent = cardInfor.name;
-        cardClone.querySelector('.card-price').textContent = cardInfor.price;
-    
-        // Hiển thị card mới
-        cardClone.style.display = 'block';
-    
-        // Thêm card mới vào grid
+        cardClone.querySelector(".card-img").src = cardInfor.image;
+        cardClone.querySelector(".card-name").textContent = cardInfor.name;
+        cardClone.querySelector(".card-price").textContent = cardInfor.price;
+        cardClone.style.display = "block";
         gridCardsContainer.appendChild(cardClone);
-    })
-    // Ẩn card mẫu
-    cardTemplate.style.display = 'none';
-    
-    const tags = document.querySelectorAll('.tags');
-    function hideTags(n) {
-        if (n > 0 && n <= tags.length) {
-            let targetTag = tags[n];
-            if (targetTag.style.display === "none") {
-                targetTag.style.display = "block"; // Hiển thị phần tử
-            } else {
-                targetTag.style.display = "none"; // Ẩn phần tử
-            }
-        } else {
-            console.log("Không tìm thấy phần tử thứ " + n);
-        }
+      });
+      cardTemplate.style.display = "none";
     }
+
+    const tags = document.querySelectorAll(".tags");
+    function hideTags(n) {
+      if (n > 0 && n <= tags.length) {
+        let targetTag = tags[n - 1];
+        if (targetTag.style.display === "none") {
+          targetTag.style.display = "block";
+        } else {
+          targetTag.style.display = "none";
+        }
+      } else {
+        console.log("Không tìm thấy phần tử thứ " + n);
+      }
+    }
+
     console.log(hideTags(3));
     console.log(hideTags(5));
     console.log(hideTags(6));
     console.log(hideTags(7));
     console.log(hideTags(8));
-}
+  }
+  {
+    // Ẩn hiển side menu
 
+    const sideMenu = document.getElementById("side-menu");
+    const showSideMenu = document.getElementById("toggle-menu");
+    const hideSideMenu = document.getElementById("close-menu");
+    const overlay = document.getElementById("overlay");
 
+    if (showSideMenu && hideSideMenu && sideMenu && overlay) {
+      showSideMenu.onclick = () => {
+        sideMenu.classList.toggle("show-side-menu");
+        overlay.style.display = "block";
+      };
+
+      hideSideMenu.onclick = () => {
+        sideMenu.classList.remove("show-side-menu");
+        overlay.style.display = "none";
+      };
+    }
+  }
+})();
+
+//function toggle cho 2 btn trong phần subheader
+
+(function () {
+  const btnShipping = document.getElementById("btn-shipping");
+  const btnTakeAway = document.getElementById("btn-take-away");
+  console.log(btnShipping, btnTakeAway)
+
+  if (btnShipping && btnTakeAway) {
+    btnShipping.onclick = () => {
+      if (
+        !btnShipping.classList.contains("btn-active") &&
+        btnTakeAway.classList.contains("btn-active")
+      )
+        btnTakeAway.classList.remove("btn-active");
+      btnShipping.classList.add("btn-active");
+    };
+    btnTakeAway.onclick = () => {
+      if (
+        btnShipping.classList.contains("btn-active") &&
+        !btnTakeAway.classList.contains("btn-active")
+      )
+        btnShipping.classList.remove("btn-active");
+      btnTakeAway.classList.add("btn-active");
+    };
+  }
+})();
